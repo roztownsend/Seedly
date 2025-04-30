b89fc465b2207b0a98035d2d3e22c5d5a108f1a0
Zonkkatul <mervezonkk@gmail.com>
2025-04-30 14:09:02 +0200

    set up Swagger UI with swagger.yaml

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger/swagger.yaml'));
 
-// route, to fill out later
-app.get('/', (_req, res) => {
-  res.send("API is running");
-});
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });

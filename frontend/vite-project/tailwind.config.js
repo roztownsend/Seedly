b278c8bc24da541837af9fe8879f90eebf2e6640
Carmelo85S <carmelo.salis85@gmail.com>
2025-05-05 11:48:00 +0200

    no custom border

--- frontend/vite-project/tailwind.config.js
+++ frontend/vite-project/tailwind.config.js
@@ -3,16 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx,css}"],
   theme: {
     extend: {
-      fontFamily: {
-        'public-sans': ['"Public Sans"', 'sans-serif'],
-      },
-      lineHeight: {
-        '80': '80px',
-        '62': '62px',
-      },
-      letterSpacing: {
-        'tightest': '-0.03em',
-      },
+      
     },
   },
 };

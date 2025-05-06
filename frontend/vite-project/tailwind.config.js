7fedbd7df8664cd4003570a34ed520fd24a23df3
Carmelo85S <carmelo.salis85@gmail.com>
2025-05-06 14:01:58 +0200

    typography style, to debug

--- frontend/vite-project/tailwind.config.js
+++ frontend/vite-project/tailwind.config.js
@@ -3,10 +3,6 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx,css}"],
   theme: {
     extend: {
-      fontFamily: {
-        'public-sans': ['"Public Sans"', 'sans-serif'],
-        'inter': ['Inter', 'sans-serif'],
-      },
     },
   },
   plugins: [],

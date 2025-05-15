ecd425efe846e490697cb85d9c93fc9c002f4999
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-05-15 17:39:13 +0200

    refactor: updated cartStore.ts to use zustand best practices

--- frontend/vite-project/src/stores/cartStore.ts
+++ frontend/vite-project/src/stores/cartStore.ts
@@ -14,9 +14,7 @@ export type CartItem = {
   quantity: number;
 };
 
-type CartState = {
-  cartItems: CartItem[];
-  cartTotal: number;
+type CartActions = {
   addItem: (itemId: CartItem) => void;
   removeItem: (id: string) => void;
   updateQuantity: (
@@ -27,50 +25,64 @@ type CartState = {
   calculateCartTotal: () => void;
 };
 
+type CartState = {
+  cartItems: CartItem[];
+  cartTotal: number;
+  actions: CartActions;
+};
+
 export const useCartStore = create<CartState>((set, get) => ({
   cartItems: [],
   cartTotal: 0,
-  addItem: (item) =>
-    set((state) => ({
-      cartItems: [...state.cartItems, item],
-    })),
+  actions: {
+    addItem: (item) =>
+      set((state) => ({
+        cartItems: [...state.cartItems, item],
+      })),
 
-  removeItem: (id) =>
-    set((state) => ({
-      cartItems: state.cartItems.filter((item) => item.id !== id),
-    })),
-  updateQuantity: (id, quantity, operation) =>
-    set((state) => {
-      const updatedCartItems = state.cartItems
-        .map((item) => {
-          if (item.id !== id) return item;
+    removeItem: (id) =>
+      set((state) => ({
+        cartItems: state.cartItems.filter((item) => item.id !== id),
+      })),
+    updateQuantity: (id, quantity, operation) =>
+      set((state) => {
+        const updatedCartItems = state.cartItems
+          .map((item) => {
+            if (item.id !== id) return item;
 
-          const newQuantity =
-            operation === "decrement"
-              ? item.quantity - quantity
-              : item.quantity + quantity;
+            const newQuantity =
+              operation === "decrement"
+                ? item.quantity - quantity
+                : item.quantity + quantity;
 
-          if (newQuantity <= 0) {
-            return null;
-          }
-          return {
-            ...item,
-            quantity: newQuantity,
-          };
-        })
-        .filter((item) => item !== null);
-      return { cartItems: updatedCartItems };
-    }),
-  calculateCartTotal: () => {
-    const items = get().cartItems;
-    if (items.length === 0) {
-      set({ cartTotal: 0 });
-      return;
-    }
-    const total = items.reduce(
-      (total, item) => total + item.price * item.quantity,
-      0
-    );
-    set({ cartTotal: total });
+            if (newQuantity <= 0) {
+              return null;
+            }
+            return {
+              ...item,
+              quantity: newQuantity,
+            };
+          })
+          .filter((item) => item !== null);
+        return { cartItems: updatedCartItems };
+      }),
+    calculateCartTotal: () => {
+      const items = get().cartItems;
+      if (items.length === 0) {
+        set({ cartTotal: 0 });
+        return;
+      }
+      const total = items.reduce(
+        (total, item) => total + item.price * item.quantity,
+        0
+      );
+      set({ cartTotal: total });
+    },
   },
 }));
+
+export const useCartItems = () => useCartStore((state) => state.cartItems);
+
+export const useCartTotal = () => useCartStore((state) => state.cartTotal);
+
+export const useCartActions = () => useCartStore((state) => state.actions);

2c0078a7f10b9ad3921434aab972c00b1593c9b8
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-05-22 13:59:00 +0200

    update: shippingOptionStore has been updated, new component ShippingOptionCard, now manages its own selected state.

--- frontend/vite-project/src/components/shipping-selector/ShippingSelector.tsx
+++ frontend/vite-project/src/components/shipping-selector/ShippingSelector.tsx
@@ -1,48 +1,23 @@
 import { useEffect } from "react";
-import './shippingSelector.css';
-import { useAllShippingOptionsStore, useShippingSelectionState } from "../../stores/shippingOptionStore";
-
+import "./shippingSelector.css";
+import {
+  useAllShippingOptions,
+  useShippingOptionsActions,
+} from "../../stores/shippingOptionStore";
+import ShippingOptionCard from "./ShippingOptionCard";
 export const ShippingSelector: React.FC = () => {
-    const { allShippingOptions, fetchAllOptions } = useAllShippingOptionsStore();
-    const { selection, setSelectionData } = useShippingSelectionState();
-    
-    
-    useEffect(() => {
-        fetchAllOptions();
-    }, [fetchAllOptions]);
-    
-    return (
-        <form className="shipping-options">
-            {allShippingOptions.map((option) => (
+  const allShippingOptions = useAllShippingOptions();
+  const { fetchAllOptions } = useShippingOptionsActions();
 
-                <div className={`shipping-options__option 
-                    ${selection === option.id ? "selection" : ""}`} key={option.id}>
-                    <div className="shipping-options__check-and-details">
-                        <div className="radio-box">
-                            <input
-                            type="radio"
-                            id={`shipping-option-${option.id}`}
-                            name="shipping-option"
-                            value={option.label}
-                            checked={selection === option.id}
-                            onChange={() => setSelectionData(option.id)}
-                            />
-                        </div>
-                    <div className="details">
-                        <div className="option--label">
-                            <label htmlFor={`shipping-option-${option.id}`}>
-                                <h4>{option.label}</h4>
-                            </label>
-                        </div>
+  useEffect(() => {
+    fetchAllOptions();
+  }, []);
 
-                        <div className="option--details">
-                            {option.min_days}-{option.max_days} business days - <strong>{option.price} kr</strong>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            ))}
-        </form>
-
-    )
-}
+  return (
+    <form className="shipping-options">
+      {allShippingOptions.map((option) => (
+        <ShippingOptionCard key={option.id} option={option} />
+      ))}
+    </form>
+  );
+};
--- frontend/vite-project/src/stores/shippingOptionStore.ts
+++ frontend/vite-project/src/stores/shippingOptionStore.ts
@@ -1,38 +1,52 @@
-import { create } from 'zustand';
-import { GetAllOptions } from '../types/shippingOptionTypes';
-import axios from 'axios';
+import { create } from "zustand";
+import { GetAllOptions } from "../types/shippingOptionTypes";
+import axios from "axios";
+import { useShallow } from "zustand/shallow";
 
 //Get all options from backend and store.
 
-type ShowAllOptions = {
-  allShippingOptions: GetAllOptions;
+type ShippingOptionsActions = {
   fetchAllOptions: () => void;
-}
+  setSelectionData: (shippingOption: string) => void;
+};
 
-export const useAllShippingOptionsStore = create<ShowAllOptions>((set) => ({
+type ShippingOptionsState = {
+  allShippingOptions: GetAllOptions;
+  selection: string;
+  actions: ShippingOptionsActions;
+};
+
+const useShippingOptionsStore = create<ShippingOptionsState>((set) => ({
   allShippingOptions: [],
-  fetchAllOptions: async () => {
-    try {
-      const response = await axios.get("http://localhost:5000/shipping-options");
-      console.log(response.data);
-      set({
-        allShippingOptions: response.data
-      });
-    } catch (error) {
-      console.error("Error searching for shipping options:", error);
-    }
-  }
+  selection: "",
+  actions: {
+    fetchAllOptions: async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/shipping-options"
+        );
+        console.log(response.data);
+        set({
+          allShippingOptions: response.data,
+        });
+      } catch (error) {
+        console.error("Unexpected error fetching shipping options", error);
+      }
+    },
+    setSelectionData: (shippingOption) => set({ selection: shippingOption }),
+  },
 }));
 
 //Save user selection (ID only) for purchase flow.
 
-type ShippingOptionId = string;
+export const useAllShippingOptions = () =>
+  useShippingOptionsStore(useShallow((state) => state.allShippingOptions));
 
-type ShippingOptionState = {
-  selection: ShippingOptionId;
-  setSelectionData: (shippingOption: ShippingOptionId) => void;
-};
+export const useSelection = () =>
+  useShippingOptionsStore((state) => state.selection);
 
-export const useShippingSelectionState = create<ShippingOptionState>((set) => ({ selection: "",
-  setSelectionData: (shippingOption) => set({ selection: shippingOption })
-}));
+export const useShippingOptionsActions = () =>
+  useShippingOptionsStore((state) => state.actions);
+
+export const useIsSelected = (optionId: string): boolean =>
+  useShippingOptionsStore((state) => state.selection === optionId);

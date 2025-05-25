fcb614985d10cfed8d0155e6310dbf9ab3dbfd6c
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-05-25 19:14:30 +0200

    add: tested adding a new public user with sequelize

--- backend/src/routes/authTest.ts
+++ backend/src/routes/authTest.ts
@@ -1,14 +1,15 @@
 //there are a few debug console.logs, shouldnt be touched
 
 import { Router, Request, Response } from "express";
-import { pool } from "../config/dbConnection";
-import { z } from "zod";
-import { Plant } from "../models/plant.model";
-import { Task } from "../models/task.model";
+import { User } from "../models/user.model";
 import {
   AuthenticatedRequest,
   authenticateUser,
 } from "../middleware/authMiddleware";
+interface UserType {
+  id: string;
+  email: string;
+}
 const router = Router();
 
 router.post(
@@ -16,6 +17,18 @@ router.post(
   authenticateUser,
   async (req: AuthenticatedRequest, res: Response) => {
     console.log(req);
+    console.log("User id - ", req.user?.id);
+    console.log("User email - ", req.user?.email);
+    if (req.user?.id && req.user.email) {
+      const newUser = await User.create({
+        id: req.user?.id,
+        email: req.user?.email,
+      });
+      console.log(newUser instanceof User);
+    } else {
+      res.json({ message: "Error creating new user" });
+    }
+
     res.json({ data: req.user?.email });
   }
 );

b74222f049b4b2e934dfebb7e35684de96dda3f1
Carmelo85S <carmelo.salis85@gmail.com>
2025-05-26 15:19:04 +0200

    some change in Stepheader style inline, shipping page has cart item integrated, adjusted a little style problem for ProductCardCart where the increment button was next to the price. Accidentaly i cancel one class...now everything is working

--- frontend/vite-project/src/pages/Shipping.tsx
+++ frontend/vite-project/src/pages/Shipping.tsx
@@ -1,41 +1,27 @@
 import ShippingForm from '../components/shipping-form/ShippingForm'
 import { OrderSummary } from '../components/order-summary/OrderSummary'
-import ProductCardCart from '../components/card-component/ProductCardCart';
-import { useCartStore } from '../stores/cartStore';
 import StepHeader from '../components/step-header/StepHeader';
+import CartItemList from '../components/cart-list-item/CartListItem';
+import './page-styles/shipping.css';
 
 const Shipping = () => {
-  const { cartItems } = useCartStore();
 
   return (
-      <div className="w-11/12 lg:w-5/6 mx-auto py-10 flex flex-col lg:flex-row gap-12">
+      <div className="shipping-container">
 
         {/* Shipping form section */}
-        <div className="p-4 w-full mx-auto max-w-lg">
+        <div className="shipping-form-section">
   
           {/* Shipping form */}
-          <div>
-            <div className="w-full lg:w-1/2 flex flex-col gap-6">
-              <StepHeader currentStep="Address" />
-            </div>
-
+            <StepHeader currentStep="Address" />
             <ShippingForm />
-          </div>
+
         </div>
 
-
         {/* Cart items and order summary section */}
-        <div className="w-full lg:w-1/2 flex flex-col gap-6 max-h-[80vh] overflow-y-auto pr-2">
+        <div className="cart-summary-section">
           {/* Cart items list */}
-          <div className="space-y-4">
-            {cartItems.map((item) => {
-              return (
-                <div key={item.id} className="list-wrapper">
-                  <ProductCardCart id={item.id} />
-                </div>
-              );
-            })}
-          </div>
+          <CartItemList /> 
 
           {/* Order summary */}
           <OrderSummary showButton={false}/>

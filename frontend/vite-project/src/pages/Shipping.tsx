7f2a7dd874699db8c06fcdb00a0087b63c044460
Carmelo85S <carmelo.salis85@gmail.com>
2025-05-26 11:14:50 +0200

    shipping form page

--- frontend/vite-project/src/pages/Shipping.tsx
+++ frontend/vite-project/src/pages/Shipping.tsx
@@ -2,34 +2,45 @@ import ShippingForm from '../components/shipping-form/ShippingForm'
 import { OrderSummary } from '../components/order-summary/OrderSummary'
 import ProductCardCart from '../components/card-component/ProductCardCart';
 import { useCartStore } from '../stores/cartStore';
+import StepHeader from '../components/step-header/StepHeader';
 
 const Shipping = () => {
   const { cartItems } = useCartStore();
 
   return (
-    <div className="w-5/6 mx-auto py-8 flex flex-col lg:flex-row gap-8">
-      {/* Shipping form section */}
-      <div className="w-full lg:w-1/2">
-        <ShippingForm />
-      </div>
+      <div className="w-11/12 lg:w-5/6 mx-auto py-10 flex flex-col lg:flex-row gap-12">
 
-      {/* Cart items and order summary section */}
-      <div className="w-full lg:w-1/2 flex flex-col gap-6 max-h-[80vh] overflow-y-auto pr-2">
-        {/* Cart items list */}
-        <div className="space-y-4">
-          {cartItems.map((item) => {
-            return (
-              <div key={item.id} className="list-wrapper">
-                <ProductCardCart id={item.id} />
-              </div>
-            );
-          })}
+        {/* Shipping form section */}
+        <div className="p-4 w-full mx-auto max-w-lg">
+  
+          {/* Shipping form */}
+          <div>
+            <div className="w-full lg:w-1/2 flex flex-col gap-6">
+              <StepHeader currentStep="Address" />
+            </div>
+
+            <ShippingForm />
+          </div>
         </div>
 
-        {/* Order summary */}
-        <OrderSummary showButton={false}/>
+
+        {/* Cart items and order summary section */}
+        <div className="w-full lg:w-1/2 flex flex-col gap-6 max-h-[80vh] overflow-y-auto pr-2">
+          {/* Cart items list */}
+          <div className="space-y-4">
+            {cartItems.map((item) => {
+              return (
+                <div key={item.id} className="list-wrapper">
+                  <ProductCardCart id={item.id} />
+                </div>
+              );
+            })}
+          </div>
+
+          {/* Order summary */}
+          <OrderSummary showButton={false}/>
+        </div>
       </div>
-    </div>
   );
 };
 

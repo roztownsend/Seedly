7b1dfbabe83bc4c372f2a8e6ca8e680d39ed5aa6
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-05-29 17:03:18 +0200

    refactor: formatted user tasks data

--- backend/src/services/getUserTasksService.ts
+++ backend/src/services/getUserTasksService.ts
@@ -4,9 +4,31 @@ import { Plant } from "../models/plant.model";
 import { User } from "../models/user.model";
 import { Purchase } from "../models/purchase.model";
 
+interface UserTaskData {
+  id: string;
+  is_completed: boolean;
+  task: {
+    id: string;
+    description: string;
+    start_month: number;
+    end_month: number;
+    plant: {
+      id: string;
+      image_url: string;
+      product_name: string;
+    };
+  };
+  user: {
+    purchases: {
+      id: string;
+      purchase_date: string;
+    };
+  };
+}
+
 export const getUserTasks = async (userId: string) => {
   try {
-    const userTaskObjects = await UserTask.findAll({
+    const userTaskObjects = (await UserTask.findAll({
       where: { user_id: userId },
       attributes: ["id", "is_completed"],
       include: [
@@ -20,7 +42,7 @@ export const getUserTasks = async (userId: string) => {
               model: Plant,
               as: "plant",
               required: false,
-              attributes: ["product_name", "image_url"],
+              attributes: ["id", "product_name", "image_url"],
             },
           ],
         },
@@ -33,16 +55,37 @@ export const getUserTasks = async (userId: string) => {
             {
               model: Purchase,
               as: "purchases",
-              attributes: ["purchase_date"],
+              attributes: ["id", "purchase_date"],
             },
           ],
         },
       ],
       raw: true,
       nest: true,
-    });
+    })) as unknown as UserTaskData[];
     console.log(JSON.stringify(userTaskObjects, null, 2));
-    return userTaskObjects;
+    const formattedTasks = userTaskObjects.map((task) => ({
+      user_task_data: {
+        id: task.id,
+        is_completed: task.is_completed,
+        task_data: {
+          id: task.task.id,
+          description: task.task.description,
+          start_month: task.task.start_month,
+          end_month: task.task.end_month,
+        },
+        plant_data: {
+          id: task.task.plant.id,
+          image_url: task.task.plant.image_url,
+          product_name: task.task.plant.product_name,
+        },
+        purchase_data: {
+          id: task.user.purchases.id,
+          purchase_date: task.user.purchases.purchase_date,
+        },
+      },
+    }));
+    return formattedTasks;
   } catch (error) {
     console.error(`Error fetching tasks for user ${userId}`);
     throw error;

8d8ad89142609d476d3a42f4827c36a44a95a8a7
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-05-29 21:07:44 +0200

    add: logic to keep track of what task info has changed and conditionally rendering a save button if something has changed from the original data

--- frontend/vite-project/src/components/purchased-seeds/SeedCard.tsx
+++ frontend/vite-project/src/components/purchased-seeds/SeedCard.tsx
@@ -2,6 +2,7 @@ import { UserTask } from "../../types/userTaskTypes";
 import { Link } from "react-router-dom";
 import { slugify } from "../../utils/slugify";
 import { formatPurchaseDate } from "../../utils/formatPurchaseDate";
+import { useState, useRef } from "react";
 
 const months = [
   "January",
@@ -29,47 +30,98 @@ const SeedCard = ({
   purchaseDate: string;
   imageUrl: string;
   tasks: UserTask[];
-}) => (
-  <div className="seed-card">
-    <div className="top-section">
-      <div className="seed-image-placeholder">
-        <img src={imageUrl} alt={name} className="object-cover w-full h-full" />
-      </div>
-      <div className="seed-info">
-        <h2>{name}</h2>
-        <p>Purchased: {formatPurchaseDate(purchaseDate)}</p>
-        <Link
-          to={`/products/${slugify(name)}`}
-          state={{ id: plantId }}
-          className="repurchase-link"
-        >
-          Re-purchase
-        </Link>
-      </div>
-    </div>
+}) => {
+  const [userTasks, setUserTasks] = useState<UserTask[]>(tasks);
+  const originalTasksRef = useRef(tasks);
+  const hasChanges =
+    JSON.stringify(userTasks) !== JSON.stringify(originalTasksRef.current);
+  console.log(hasChanges);
 
-    <table className="seed-task-table">
-      <thead>
-        <tr>
-          <th>Task</th>
-          <th>Completed</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tasks.map((task, index) => (
-          <tr key={index}>
-            <td>
-              <strong>{months[task.start_month]} to </strong>
-              <strong>{months[task.end_month]}</strong> - {task.description}
-            </td>
-            <td>
-              <input type="checkbox" />
-            </td>
+  const toggleTaskCompletion = (taskId: string) => {
+    setUserTasks((prevState) =>
+      prevState.map((task) =>
+        task.task_id === taskId
+          ? { ...task, is_completed: !task.is_completed }
+          : task
+      )
+    );
+  };
+  const getChangedTasks = () => {
+    return userTasks.filter((currentTask) => {
+      const originalTask = originalTasksRef.current.find(
+        (task) => task.user_task_id === currentTask.user_task_id
+      );
+      return (
+        originalTask && originalTask.is_completed !== currentTask.is_completed
+      );
+    });
+  };
+
+  const saveChanges = () => {
+    const tasksToUpdate = getChangedTasks();
+    console.log(tasksToUpdate);
+  };
+
+  console.log(userTasks);
+  return (
+    <div className="seed-card">
+      <div className="top-section">
+        <div className="seed-image-placeholder">
+          <img
+            src={imageUrl}
+            alt={name}
+            className="object-cover w-full h-full"
+          />
+        </div>
+        <div className="seed-info">
+          <h2>{name}</h2>
+          <p>Purchased: {formatPurchaseDate(purchaseDate)}</p>
+          <Link
+            to={`/products/${slugify(name)}`}
+            state={{ id: plantId }}
+            className="repurchase-link"
+          >
+            Re-purchase
+          </Link>
+        </div>
+      </div>
+
+      <table className="seed-task-table">
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Completed</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+        </thead>
+        <tbody>
+          {userTasks.map((task, index) => (
+            <tr key={index}>
+              <td>
+                <strong>{months[task.start_month - 1]} to </strong>
+                <strong>{months[task.end_month - 1]}</strong> -{" "}
+                {task.description}
+              </td>
+              <td>
+                <input
+                  type="checkbox"
+                  checked={task.is_completed}
+                  onChange={() => toggleTaskCompletion(task.task_id)}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {hasChanges && (
+        <button
+          onClick={saveChanges}
+          className="bg-green-800 text-white text-base mt-3 p-2"
+        >
+          Save changes
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default SeedCard;

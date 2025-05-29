b4b48b3bce7e2df22ca4d1b2eadf43927a185587
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-05-29 17:06:32 +0200

    add: userTaskTypes file

--- frontend/vite-project/src/pages/DashBoard.tsx
+++ frontend/vite-project/src/pages/DashBoard.tsx
@@ -2,36 +2,13 @@ import { useEffect, useState } from "react";
 import PurchasedSeeds from "../components/purchased-seeds/PurchasedSeeds";
 import axios from "axios";
 import { useAuthSession, useAuthLoading } from "../stores/authStore";
+import { UserTaskData, UserTasksResponse } from "../types/userTaskTypes";
 
 function DashBoard() {
   const session = useAuthSession();
   const isLoading = useAuthLoading();
   const [userTasks, setUserTask] = useState<UserTaskData[]>([]);
 
-  type UserTaskData = {
-    user_task_data: {
-      id: string;
-      is_completed: boolean;
-    };
-    task_data: {
-      id: string;
-      description: string;
-      start_month: string;
-      end_month: string;
-    };
-    plant_data: {
-      id: string;
-      image_url: string;
-      product_name: string;
-    };
-    purchase_data: {
-      id: string;
-      purchase_date: string;
-    };
-  };
-  type UserTasksResponse = {
-    tasks: UserTaskData[];
-  };
   useEffect(() => {
     const fetchUserTasks = async () => {
       try {

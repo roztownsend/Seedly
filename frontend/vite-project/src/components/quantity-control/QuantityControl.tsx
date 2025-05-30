79f0ba8ea4b8bf47c31bdb836e30c1f5de135c9e
Roz Townsend <rdtownsend@protonmail.com>
2025-05-30 17:07:20 +0200

    Add tasks for items 40-60; tiny css cleanup

--- frontend/vite-project/src/components/quantity-control/QuantityControl.tsx
+++ frontend/vite-project/src/components/quantity-control/QuantityControl.tsx
@@ -13,7 +13,7 @@ export const QuantityControl: React.FC<QuantityControlProps> = ({
   const { updateQuantity } = useCartActions();
   if (!cartItem) return fallbackButton ?? null;
   return (
-    <div className="flex gap-3 items-center justify-between border border-black px-3 py-1">
+    <div className="quantity-control">
       <button onClick={() => updateQuantity(cartId, 1, "decrement")}>-</button>
       <span>{cartItem?.quantity}</span>
       <button onClick={() => updateQuantity(cartId, 1, "increment")}>+</button>

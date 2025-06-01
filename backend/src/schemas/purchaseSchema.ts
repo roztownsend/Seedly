f0155eb7ac23b85ac220ad5c6c6a157810b1f44d
Roz Townsend <rdtownsend@protonmail.com>
2025-06-01 14:02:51 +0200

    Add shipping and purchaseItem to schema

--- backend/src/schemas/purchaseSchema.ts
+++ backend/src/schemas/purchaseSchema.ts
@@ -1,8 +1,24 @@
 import { z } from 'zod';
 
+export const purchaseItemSchema = z.object({
+  quantity: z.number().positive(),
+  plantId: z.string()
+});
+
+export const shippingInfoSchema = z.object({
+  name: z.string(),
+  email: z.string(),
+  address: z.string(),
+  apartment: z.string().optional(),
+  postalCode: z.number(),
+  city: z.string()
+});
+
 export const purchaseSchema = z.object({
   userId: z.string(),
   totalItems: z.number().positive(),
   shippingPrice: z.number().positive(),
-  totalAmount: z.number().positive()
+  totalAmount: z.number().positive(),
+  paymentMethod: z.string()
   });
+

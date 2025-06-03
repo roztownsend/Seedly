a4e60e09111260bccf757ee07d08d185ea8fdd70
João Canabarro Campos <joaocampos963@icloud.com>
2025-06-03 11:54:08 +0200

    Update server

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -12,39 +12,40 @@ const yamljs_1 = __importDefault(require("yamljs"));
 const path_1 = __importDefault(require("path"));
 const sequelizeConnect_1 = __importDefault(require("./config/sequelizeConnect"));
 const initModels_1 = require("./models/initModels");
+const plantRoutes_1 = __importDefault(require("./routes/plantRoutes"));
+const shippingRoutes_1 = __importDefault(require("./routes/shippingRoutes"));
+const userTaskRoutes_1 = __importDefault(require("./routes/userTaskRoutes"));
+const authTest_1 = __importDefault(require("./routes/authTest"));
+const purchaseRoutes_1 = __importDefault(require("./routes/purchaseRoutes"));
+//testing server startup
+sequelizeConnect_1.default
+    .authenticate()
+    .then(() => {
+    console.log("Connected to Supabase via Sequelize");
+    (0, initModels_1.initModels)(sequelizeConnect_1.default);
+    // plantsInserter()
+    // .then(() => console.log("yay"))
+    // .catch(() => console.log("boo"));
+})
+    .catch((err) => console.error("Sequelize connection error:", err));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
-(async () => {
-    try {
-        await sequelizeConnect_1.default.authenticate();
-        console.log("Connected to Supabase via Sequelize");
-        (0, initModels_1.initModels)(sequelizeConnect_1.default);
-        const plantRoutes = require("./routes/plantRoutes").default;
-        const shippingRoutes = require("./routes/shippingRoutes").default;
-        const userTaskRoutes = require("./routes/userTaskRoutes").default;
-        const purchaseRoutes = require("./routes/purchaseRoutes").default;
-        const authTest = require("./routes/authTest").default;
-        app.use((0, cors_1.default)());
-        app.use(express_1.default.json());
-        app.use("/plants", plantRoutes);
-        app.use("/shipping-options", shippingRoutes);
-        app.use("/auth-test", authTest);
-        app.use("/user-tasks", userTaskRoutes);
-        app.use("/purchase", purchaseRoutes);
-        const swaggerDocument = yamljs_1.default.load(path_1.default.join(__dirname, "../swagger/swagger.yaml"));
-        app.get("/", (_req, res) => {
-            res.send("API is running");
-        });
-        app.use("/docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocument));
-        if (process.env.NODE_ENV !== "production") {
-            app.listen(PORT, () => {
-                console.log(`Server running on http://localhost:${PORT}`);
-            });
-        }
-    }
-    catch (err) {
-        console.error("Sequelize connection error:", err);
-        throw err;
-    }
-})();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use("/plants", plantRoutes_1.default);
+app.use("/shipping-options", shippingRoutes_1.default);
+app.use("/auth-test", authTest_1.default);
+app.use("/user-tasks", userTaskRoutes_1.default);
+app.use("/purchase", purchaseRoutes_1.default);
+const swaggerDocument = yamljs_1.default.load(path_1.default.join(__dirname, "../swagger/swagger.yaml"));
+// basic route, look /routes folder for actual ones
+app.get("/", (_req, res) => {
+    res.send("API is running");
+});
+app.use("/docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocument));
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
 exports.default = app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,49 +8,56 @@ import YAML from "yamljs";
 import path from "path";
 import sequelize from "./config/sequelizeConnect";
 import { initModels } from "./models/initModels";
+import plantRoutes from "./routes/plantRoutes";
+import shippingRoutes from "./routes/shippingRoutes";
+import userTaskRoutes from "./routes/userTaskRoutes";
+import { Purchase } from "./models/purchase.model";
+import { Plant } from "./models/plant.model";
+import { ShippingOption } from "./models/shippingOption.model";
+import plantsInserter from "./utils/plantsInserterHelper";
+import optionsInserter from "./utils/optionsInserterHelper";
+import authTest from "./routes/authTest";
+import { makeUserAdmin } from "./services/makeUserAdmin";
+
+import purchaseRoutes from "./routes/purchaseRoutes";
+//testing server startup
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connected to Supabase via Sequelize");
+    initModels(sequelize);
+    // plantsInserter()
+    // .then(() => console.log("yay"))
+    // .catch(() => console.log("boo"));
+  })
+  .catch((err) => console.error("Sequelize connection error:", err));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+app.use("/plants", plantRoutes);
+app.use("/shipping-options", shippingRoutes);
+app.use("/auth-test", authTest);
+app.use("/user-tasks", userTaskRoutes);
+app.use("/purchase", purchaseRoutes);
 
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connected to Supabase via Sequelize");
-    initModels(sequelize);
+const swaggerDocument = YAML.load(
+  path.join(__dirname, "../swagger/swagger.yaml")
+);
 
-    const plantRoutes = require("./routes/plantRoutes").default;
-    const shippingRoutes = require("./routes/shippingRoutes").default;
-    const userTaskRoutes = require("./routes/userTaskRoutes").default;
-    const purchaseRoutes = require("./routes/purchaseRoutes").default;
-    const authTest = require("./routes/authTest").default;
+// basic route, look /routes folder for actual ones
+app.get("/", (_req, res) => {
+  res.send("API is running");
+});
 
-    app.use(cors());
-    app.use(express.json());
-    app.use("/plants", plantRoutes);
-    app.use("/shipping-options", shippingRoutes);
-    app.use("/auth-test", authTest);
-    app.use("/user-tasks", userTaskRoutes);
-    app.use("/purchase", purchaseRoutes);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-    const swaggerDocument = YAML.load(
-      path.join(__dirname, "../swagger/swagger.yaml")
-    );
-
-    app.get("/", (_req, res) => {
-      res.send("API is running");
-    });
-
-    app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-    if (process.env.NODE_ENV !== "production") {
-      app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
-      });
-    }
-  } catch (err) {
-    console.error("Sequelize connection error:", err);
-    throw err;
-  }
-})();
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
 
 export default app;
